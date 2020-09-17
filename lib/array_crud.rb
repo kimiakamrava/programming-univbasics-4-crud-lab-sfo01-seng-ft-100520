cfa5e374c949e5ec76bd3c21730cf69a4a21a8b7
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-17 06:29:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
 array = ["wow", "I", "am", "really", "learning"]
-  element = "array's"
+  element = "arrays"
+  array.push "arrays"
 end
 
 def add_element_to_start_of_array(array, element)
